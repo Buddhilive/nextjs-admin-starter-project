8a0d3ad7e70d73ec2cac5444cc66eabfd69bf9ac
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-06-03 22:00:00 +0530

    relocate Angular Firebase Config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,7 +6,7 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import { FIREBASE_CONFIG } from './firebase.config';
+import { FIREBASE_CONFIG } from '../../.solias/firebase.config';
 
 export const appConfig: ApplicationConfig = {
   providers: [
