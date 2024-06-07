65101dd7600d8bb7472da9829bdd13849a0a1b9b
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-06-07 22:00:00 +0530

    add navigation component

--- src/app/public/index-container/index-container.component.ts
+++ src/app/public/index-container/index-container.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
+import { NavigationComponent } from '../../components/navigation/navigation.component';
 
 @Component({
   selector: 'solias-index-container',
   standalone: true,
-  imports: [],
+  imports: [
+    HeaderComponent,
+    NavigationComponent
+  ],
   templateUrl: './index-container.component.html',
   styleUrl: './index-container.component.scss'
 })
