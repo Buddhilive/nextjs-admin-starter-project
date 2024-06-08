e3c35134537a2f19ae492dcbb926e103f8cbf746
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-06-08 22:00:00 +0530

    add footer component

--- src/app/public/index-container/index-container.component.ts
+++ src/app/public/index-container/index-container.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { NavigationComponent } from '../../components/navigation/navigation.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'solias-index-container',
   standalone: true,
   imports: [
     HeaderComponent,
-    NavigationComponent
+    NavigationComponent,
+    FooterComponent
   ],
   templateUrl: './index-container.component.html',
   styleUrl: './index-container.component.scss'
