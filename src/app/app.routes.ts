a1054b18a4d237faa45d650f683700877225a962
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-06-09 22:00:00 +0530

    add portal dashboard component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { IndexContainerComponent } from './public/index-container/index-container.component';
+import { AuthDashboardComponent } from './auth/auth-dashboard/auth-dashboard.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: IndexContainerComponent
+    },
+    {
+        path: 'portal',
+        component: AuthDashboardComponent
     }
 ];
---
+++ src/app/auth/auth-dashboard/auth-dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'solias-auth-dashboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './auth-dashboard.component.html',
+  styleUrl: './auth-dashboard.component.scss'
+})
+export class AuthDashboardComponent {
+
+}
