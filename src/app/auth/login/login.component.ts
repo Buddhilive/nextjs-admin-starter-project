cc4b21168fc87f542873bdbc932532045ddb93f2
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-06-22 22:13:25 +0530

    update login component

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'solias-login',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButtonModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  constructor(private authService: AuthService) {}
 
+  login() {
+    this.authService.GoogleAuth();
+  }
 }
