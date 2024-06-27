eb3eefed68547885496f37a988a7ef182e5fc612
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-06-27 22:00:00 +0530

    add verify email component

---
+++ src/app/auth/verify-email/verify-email.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'solias-verify-email',
+  standalone: true,
+  imports: [],
+  templateUrl: './verify-email.component.html',
+  styleUrl: './verify-email.component.scss'
+})
+export class VerifyEmailComponent {
+
+}
