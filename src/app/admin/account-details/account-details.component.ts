71ab5cca368d91e60dee4cf4db2f629c1ddf4caa
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-07-04 22:00:00 +0530

    add account details component

---
+++ src/app/admin/account-details/account-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'solias-account-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './account-details.component.html',
+  styleUrl: './account-details.component.scss'
+})
+export class AccountDetailsComponent {
+
+}
